8ad9fa0feb8a650127df2173a3e66033d0adf278
ryab0911 <baaska479@gmail.com>
2023-10-16 16:18:17 +0800

    styles

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const postCSSPlugins = [
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer"),
+];
+module.exports = {
+  entry: "/app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "app"),
+      watch: true,
+    },
+    liveReload: false,
+    compress: true,
+    port: 9099,
+    open: true,
+    hot: true,
+    host: "0.0.0.0",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: { postcssOptions: { plugins: postCSSPlugins } },
+          },
+        ],
+      },
+    ],
+  },
+};
