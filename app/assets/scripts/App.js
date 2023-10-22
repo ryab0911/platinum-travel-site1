edbab4da76eaa00651d118bd21251e72f7c410e9
ryab0911 <baaska479@gmail.com>
2023-10-22 15:41:41 +0800

    sticky headeriig buren hiij duusgallaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import "../styles/style.css";
 import MobileMenu from "./modules/mobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
+
+let stickyHeader = new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
